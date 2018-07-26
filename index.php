0d86f6b7b3559afe6d2f1a0b211cecaf0a120ba1
dominiquem <dominique.meaux@gmail.com>
2018-07-26 09:29:23 +0200

    php p1exo8

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>p1exo8</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $nombre = 3 + 4;
+          echo $nombre;
+      ?>
+    </p>
+    <p>
+      <?php
+        $nombre1 = 5 * 20;
+          echo $nombre1;
+      ?>
+    </p>
+    <p>
+      <?php
+        $nombre2 = 45 / 5;
+          echo $nombre2;
+      ?>
+    </p>
+  </body>
+</html>
